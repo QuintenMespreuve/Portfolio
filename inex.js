8a83a5b5696eb22b9490458f180d0f57cc44285b
Quinten Mespreuve <74731706+QuintenMespreuve@users.noreply.github.com>
2024-05-31 18:07:35 +0000

    portfolio

---
+++ inex.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Bedankt voor je bericht, we nemen zo snel mogelijk contact met je op!');
+});
